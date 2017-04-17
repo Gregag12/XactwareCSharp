f6795c7f905e9e490a16390044eca103cfe0a8df
Gregag12 <gagarrett@xactware.com>
2017-04-17 09:56:22 -0600

    Chapter 12
    Completed Chapter 12 projects

---
+++ CSharpPlayersGuide/Chapter 12 - Looping/FizzBuzz.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter_12___Looping
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+
+            //Things needed in a for loop = for(initial condition; condition to check; action at end of loop)
+
+            //for (int row = 0; row < 5; row++)
+            //{
+            //    for (int column = 0; column < 10; column++)
+            //        Console.Write("*");
+            //    Console.WriteLine(); // This makes it wrap around to the beginning of the line.
+
+            //}
+            //Console.ReadKey();
+
+            for (int number = 1; number < 101; number++)
+            {
+                if (number % 3 == 0 && number % 5 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (number % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (number % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else
+                    Console.WriteLine(number);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpPlayersGuide/Chapter 12 - Looping/Print_a_Pyramid.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//namespace Chapter_12___Looping
+//{
+//    class Print_a_Pyramid
+//    {
+//        static void Main(string[] args)
+//        {
+
+//            Things needed in a for loop = for (initial condition; condition to check; action at end of loop)
+
+//            for (int row = 0; row < 5; row++)
+//            {
+//                for (int column = 0; column < 10; column++)
+//                    Console.Write("*");
+//                Console.WriteLine(); // This makes it wrap around to the beginning of the line.
+
+//            }
+//            Console.ReadKey();
+
+//            for (int row = 0; row < 5; row++)
+//            {
+//                for (int space = 0; space < 5 - row - 1; space++)
+//                {
+//                    Console.Write(" ");
+//                }
+//                for (int star = 0; star < row * 2 + 1; star++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+//            Console.ReadKey();
+//        }
+
+//    }
+//}
